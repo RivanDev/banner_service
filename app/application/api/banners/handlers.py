05adc4e9934fe46db969a48a06d8f0b966194037
rivan <rivan_git@mail.ru>
2024-04-09 21:19:40 +0300

    refactoring

--- app/application/api/banners/handlers.py
+++ app/application/api/banners/handlers.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter, Header
 
 from .schemas import BannerOut
 
-banner_router = APIRouter(prefix="/banner")
+banner_router = APIRouter()
 
 
 @banner_router.post("/", response_model=BannerOut, summary="Создание нового баннера")
-async def create_banner(token: str = Header(..., description="Токен админа")): ...
+async def create_banner(
+    token: str = Header(description="Токен админа", json_schema_extra={"example": "admin_token"})
+): ...
--- app/application/db/models/tag.py
+++ app/application/db/models/tag.py
@@ -1,7 +1,11 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from .banner import Banner
 from .base import Base
+
+if TYPE_CHECKING:
+    from .banner import Banner
 
 
 class Tag(Base):
